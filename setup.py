9b74a5907e620f783fddbe9f28c72f0c2ffae59e
Vu Anh <brother.rain.1024@gmail.com>
2017-03-24 11:56:07 +0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,39 @@
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+setup(
+    name='underthesea.word_sent',
+    version='1.0.0',
+    description="Word Segmentation Experiment",
+    long_description=readme + '\n\n' + history,
+    author="Vu Anh",
+    author_email='brother.rain.1024@gmail.com',
+    url='https://github.com/magizbox/underthesea',
+    packages=[
+        'underthesea',
+    ],
+    package_dir={'underthesea': 'underthesea'},
+    entry_points={
+        'console_scripts': [
+            'underthesea=underthesea.cli:main'
+        ]
+    },
+    include_package_data=True,
+    install_requires=requirements,
+    license="GNU General Public License v3",
+    zip_safe=False,
+    keywords='underthesea',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
