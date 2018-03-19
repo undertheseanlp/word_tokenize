06e80ff3463ac3df44f40155585cc566454255fb
anhv <anhv.ict91@gmail.com>
2018-03-19 18:14:02 +0700

    refactor

--- experiments/crf/score_.py
+++ experiments/crf/score_.py
@@ -1,17 +1,44 @@
-def precision(y_true, y_pred):
-    labels_true = set(y_true).intersection(y_pred)
-    if len(y_pred) == 0:
+def create_tags(sentences):
+    tags = []
+    for sentence_index, iob_tags in enumerate(sentences):
+        i = 0
+        current_tag = [None, None, None, None]
+        while i < len(iob_tags):
+            if iob_tags[i] == "O" and current_tag[0]:
+                tags.append(tuple(current_tag))
+                current_tag = [None, None, None, None]
+            elif iob_tags[i].startswith("B"):
+                current_tag[0] = iob_tags[i][2:]
+                current_tag[1] = i
+                current_tag[2] = i
+                current_tag[3] = sentence_index
+            elif iob_tags[i].startswith("I"):
+                current_tag[2] = i
+            i += 1
+        if current_tag[0]:
+            tags.append(tuple(current_tag))
+    tags = set(tags)
+    return tags
+
+
+def iob_score(y_true, y_pred):
+    """
+    Parameters
+    ----------
+    y_true : list
+        each sublist is a list of tags
+    y_pred: list of list
+        each sublist is a list of tags
+    """
+    tags_true = create_tags(y_true)
+    tags_pred = create_tags(y_pred)
+    ref = len(tags_true)
+    true = len(set(tags_true).intersection(tags_pred))
+    sys = len(tags_pred)
+    if ref == 0 or true == 0 or sys == 0:
         return 0
-    else:
-        return len(labels_true) / len(y_pred)
+    p = true / sys
+    r = true / ref
+    f1 = (2 * p * r) / (p + r)
 
-
-def recall(y_true, y_pred):
-    labels_true = set(y_true).intersection(y_pred)
-    return len(labels_true) / len(y_pred)
-
-
-def f1_score(y_true, y_pred):
-    p = precision(y_true, y_pred)
-    r = recall(y_true, y_pred)
-    return 2 * (p * r) / (p + r)
+    return f1
