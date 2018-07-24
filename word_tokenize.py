6a403837d5285cb53062b175ba211f3f5b7b7000
Vu Anh <anhv.ict91@gmail.com>
2018-07-24 11:06:23 +0700

    add predict file option

--- word_tokenize.py
+++ word_tokenize.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 
 from util.crf.word_tokenize import word_tokenize
 
@@ -8,6 +9,7 @@ text_group.add_argument("--text", metavar="TEXT", help="text to predict")
 file_group = parser.add_argument_group("The following arguments are mandatory for file option")
 file_group.add_argument("--fin", help="file input")
 file_group.add_argument("--fout", help="file output")
+parser.add_argument("--model", help="path to saved model")
 
 args = parser.parse_args()
 
@@ -19,3 +21,18 @@ if __name__ == '__main__':
         text = args.text
         label = word_tokenize(text, format="text")
         print(label)
+
+    if args.fin or args.fout:
+        if not (args.fout and args.fin):
+            parser.error("Options --fin and --fout must be set together")
+        file_in = args.fin
+        file_out = args.fout
+        try:
+            os.rm(args.fout)
+        except:
+            pass
+        f = open(file_out, "a")
+        for text in open(file_in):
+            text = text.strip()
+            output = word_tokenize(text, format="text") + "\n"
+            f.write(output)
