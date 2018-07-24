b187790cfe43896a60a439f95cb907c59c18cd7b
Vu Anh <anhv.ict91@gmail.com>
2018-07-24 10:17:59 +0700

    update requirements

--- tests/test_train.py
+++ tests/test_train.py
@@ -1,9 +1,14 @@
+# Preprocess data
+#   python util/preprocess_vlsp2016.py --sample 100 --output util/vslp2016_100
+# before run tests
+
 from os.path import dirname, join
 from unittest import TestCase
 
 from util.crf.train import train
 
 TMP_FOLDER = join(dirname(dirname(__file__)), "tmp")
+
 class TestTrain(TestCase):
     def test_train(self):
         train_path = join(TMP_FOLDER, "vlsp2016_100", "train.txt")
