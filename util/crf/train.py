f5ade45c67639f6b3d04a713eefd79da868b5b86
Vu Anh <anhv.ict91@gmail.com>
2018-08-10 13:34:48 +0700

    refactore features

--- util/crf/train.py
+++ util/crf/train.py
@@ -1,11 +1,11 @@
 from os import makedirs
-from os.path import join, dirname
+from os.path import dirname
 from languageflow.model.crf import CRF
 
 from util.crf.word_tokenize import CRFModel
+from util.crf.word_tokenize.features import template
 from .load_data import load_dataset
 from .transformer.custom_transformer import CustomTransformer
-from .feature_template import template
 
 
 def train(train_path, model_path):
@@ -34,6 +34,15 @@ def train(train_path, model_path):
 
 def train_test(train_path, test_path):
     model_path = "model.tmp.bin"
+    output_path = "output.txt"
+    output = open(output_path, "a")
     train(train_path, model_path)
     estimator = CRFModel.instance(model_path)
+
+
+    test = load_dataset(test_path)
+    for sample in test:
+        sentence = [token[0] for token in sample]
+        y_test = [token[1] for token in sample]
+        y_pred = estimator.predict(sentence)
     print(0)
