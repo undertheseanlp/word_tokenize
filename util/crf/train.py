72644a21f395bd06bd5a2adde86778cdd377edf3
Vu Anh <anhv.ict91@gmail.com>
2018-08-11 15:05:46 +0700

    add train-test

--- util/crf/train.py
+++ util/crf/train.py
@@ -1,7 +1,9 @@
+import os
 from os import makedirs
 from os.path import dirname
 from languageflow.model.crf import CRF
 
+from util.crf.conlleval import evaluate, parse_args
 from util.crf.word_tokenize import CRFModel
 from util.crf.word_tokenize.features import template
 from .load_data import load_dataset
@@ -32,17 +34,38 @@ def train(train_path, model_path):
     estimator = CRF(params=params, filename=model_path)
     estimator.fit(X, y)
 
+
+def _remove_file(output_path):
+    try:
+        os.remove(output_path)
+    except:
+        pass
+
+
 def train_test(train_path, test_path):
     model_path = "model.tmp.bin"
     output_path = "output.txt"
+    _remove_file(output_path)
     output = open(output_path, "a")
     train(train_path, model_path)
     estimator = CRFModel.instance(model_path)
-
 
     test = load_dataset(test_path)
     for sample in test:
         sentence = [token[0] for token in sample]
         y_test = [token[1] for token in sample]
         y_pred = estimator.predict(sentence)
-    print(0)
+        for i in range(len(y_test)):
+            line = "{}\t{}\t{}\n".format(y_pred[i][0], y_test[i], y_pred[i][1])
+            output.write(line)
+        output.write("\n")
+
+    class Args(object):
+        pass
+
+    args = Args()
+    args.latex = False
+    args.raw = False
+    args.delimiter = None
+    args.oTag = "O"
+    evaluate(open(output_path), args)
