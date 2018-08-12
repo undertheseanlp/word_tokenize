69510827c20dd2ec7e1f223618cc400b094adf9f
anhv <anhv.ict91@gmail.com>
2018-08-12 12:20:33 +0700

    update features

--- util/crf/word_tokenize/features.py
+++ util/crf/word_tokenize/features.py
@@ -1,15 +1,13 @@
 template = [
     "T[-2].lower", "T[-1].lower", "T[0].lower", "T[1].lower", "T[2].lower",
+
     "T[-1].isdigit", "T[0].isdigit", "T[1].isdigit",
 
     "T[-1].istitle", "T[0].istitle", "T[1].istitle",
-
     "T[0,1].istitle", "T[0,2].istitle",
 
     "T[-2].is_in_dict", "T[-1].is_in_dict", "T[0].is_in_dict", "T[1].is_in_dict", "T[2].is_in_dict",
-
     "T[-2,-1].is_in_dict", "T[-1,0].is_in_dict", "T[0,1].is_in_dict", "T[1,2].is_in_dict",
-
     "T[-2,0].is_in_dict", "T[-1,1].is_in_dict", "T[0,2].is_in_dict",
 
     # word unigram and bigram and trigram
